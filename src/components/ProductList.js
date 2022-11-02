359bf50d0664822cde0d7bcb3bccec84753e7349
chaaerim <cofla411@ewhain.net>
2022-11-03 02:13:26 +0900

    :sparkles: feat: main 페이지 작업 완료

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,20 +3,26 @@ import Component from '../core/Component.js';
 export default class ProductList extends Component {
   template() {
     const filteredItems = this.$props;
-    console.log(filteredItems);
+    console.log(filteredItems[0].imageUrl);
     return `
-      <ul>
+      <div>
+        <div class="title">상품 목록</div>
+        <div class="product-area">
         ${filteredItems
           .map(
             (filteredItem) => `
-          <li data-seq="${filteredItem.id}}">
-            ${filteredItem.name}
-           
-          </li>
+            <div class="product">
+            <img src="${filteredItem.imageUrl}" class="product-img"/>
+            <span class="product-name">${filteredItem.name}</span>
+            <span class="price">${filteredItem.price
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원~</span>
+          </div>
         `
           )
           .join('')}
-      </ul>
+          </div>
+      </div>
     `;
   }
 
