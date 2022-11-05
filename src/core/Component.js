e72bc8159aed9c849e88fe6c1d98bec1ec638096
ding-co <pangoons@naver.com>
2022-11-05 19:53:02 +0900

    [UPDATE] core/Component - basic ver.

--- src/core/Component.js
+++ src/core/Component.js
@@ -1,5 +1,3 @@
-import { observable, observe } from './observer.js';
-
 export default class Component {
   $target;
   $props;
@@ -8,41 +6,27 @@ export default class Component {
     this.$target = $target;
     this.$props = $props;
     this.setup();
-    // -- basic --
-    // this.setEvent();
-    // this.render();
+    this.setEvent();
+    this.render();
   }
-  setup() {
-    this.$state = observable(this.initState());
-    observe(() => {
-      this.render();
-      this.setEvent();
-      this.mounted();
-    });
-  }
+  setup() {}
   mounted() {}
-  initState() {
-    return {};
-  }
   template() {
     return '';
   }
   render() {
     this.$target.innerHTML = this.template();
-    // -- basic --
-    // this.mounted();
+    this.mounted();
   }
   setEvent() {}
-
-  // -- basic --
-  // setState(newState) {
-  //   this.$state = { ...this.$state, ...newState };
-  //   this.render();
-  // }
-  // addEvent(eventType, selector, callback) {
-  //   this.$target.addEventListener(eventType, (event) => {
-  //     if (!event.target.closest(selector)) return false;
-  //     callback(event);
-  //   });
-  // }
+  setState(newState) {
+    this.$state = { ...this.$state, ...newState };
+    this.render();
+  }
+  addEvent(eventType, selector, callback) {
+    this.$target.addEventListener(eventType, (event) => {
+      if (!event.target.closest(selector)) return false;
+      callback(event);
+    });
+  }
 }
